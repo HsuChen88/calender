48d885bcde3a99666c7edb9b72d5395e7e0e9592
HsuChen88 <shine95763@gmail.com>
2022-07-05 10:29:08 +0800

    debug everyday.py

--- everyday.py
+++ everyday.py
@@ -41,77 +41,79 @@ pattern = f'7月{day}日'
 for x in range(1, ws.max_column+1):
     char = get_column_letter(x)
     getdate = ws[char + '1'].value
-
     if getdate == pattern:
         col_num = x
         break
-    else:
-        print("Today is not on the Calender.")
-        break
+    col_num = None
 
-task_list = []
-char = get_column_letter(col_num)
 
-i = 2
-task = ws[char + str(i)].value
-while (task != '-'):    # Calender每天所有事情最後會以'-'結尾
-    if task == None:
-        i += 1
-        task = ws[char + str(i)].value
-        continue
-    else:  
-        task_list.append(task)
-        i += 1
-        task = ws[char + str(i)].value
+if col_num == None:
+    print("Today is not on the Calender.")
+else:
+    task_list = []
+    char = get_column_letter(col_num)
 
-# 讀 count.txt 取得 "是否今日第一次打開" 及 "各項課程的記數"
-count_list = []
-with open("C:/Users/User/Desktop/calender/count.txt", mode="r", encoding="utf-8") as file:
-    data  = file.read()
-sp_list = re.split("\n", data)
-for item in sp_list:
-    tmp_list = re.split("(?:=)", item)   
-    count_list.append(tmp_list)
+    i = 2
+    task = ws[char + str(i)].value
+    while (task != '-'):    # Calender每天所有事情最後會以'-'結尾
+        if task == None:
+            i += 1
+            task = ws[char + str(i)].value
+            continue
+        else:  
+            task_list.append(task)
+            i += 1
+            task = ws[char + str(i)].value
+
+    # 讀 count.txt 取得 "是否今日第一次打開" 及 "各項課程的記數"
+    count_list = []
+    with open("C:/Users/User/Desktop/calender/count.txt", mode="r", encoding="utf-8") as file:
+        data  = file.read()
+    sp_list = re.split("\n", data)
+    for item in sp_list:
+        tmp_list = re.split("(?:=)", item)   
+        count_list.append(tmp_list)
+        
+    get_today = int(count_list[0][1])
+    DS_count = int(count_list[1][1])
+    Discrete_count = int(count_list[2][1])
+    CD_count = int(count_list[3][1])
+
+    # 若第一次打開 => 記數+1
+    for item in task_list:
+        if (get_today != day):
+            if item == "資結":
+                DS_count += 1
+            if item == "離散":
+                Discrete_count += 1
+            if item == "計概":
+                CD_count += 1
+                
+    # 寫入新的記數到 count.txt
+    doc = f'Today = {dateList[2]}\nDS_count = {DS_count}\nDiscrete_count = {Discrete_count}\nCD_count = {CD_count}'
+    with open("C:/Users/User/Desktop/calender/count.txt", mode="w", encoding="utf-8") as file:
+            file.write(doc)
+
+    # 取得各項行程的"時間長度"存放於 time_list
+    count = 0
+    time_list = []
+    for item in task_list:
+        obj = Event(item)
+        if (item == "離散"): count = Discrete_count
+        elif (item == "資結"): count = DS_count
+        elif (item == "計概"): count = CD_count
+        time_list.append(obj.GetTime(count))
     
-get_today = int(count_list[0][1])
-DS_count = int(count_list[1][1])
-Discrete_count = int(count_list[2][1])
-CD_count = int(count_list[3][1])
+    # output
+    task_count =  len(task_list)
+    print(f'今天有 {task_count} 項任務!\n')
+    for i in range(task_count):
+        print(str(i+1) + ". " + task_list[i])
+        if time_list[i] == []:
+            print("")
+        else:
+            print(time_list[i])
+            print("")        
+    print("")
 
-# 若第一次打開 => 記數+1
-for item in task_list:
-    if (get_today != day):
-        if item == "資結":
-            DS_count += 1
-        if item == "離散":
-            Discrete_count += 1
-        if item == "計概":
-            CD_count += 1
-
-# 寫入新的記數到 count.txt
-doc = f'Today = {dateList[2]}\nDS_count = {DS_count}\nDiscrete_count = {Discrete_count}\nCD_count = {CD_count}'
-with open("C:/Users/User/Desktop/calender/count.txt", mode="w", encoding="utf-8") as file:
-        file.write(doc)
-
-# 取得各項行程的"時間長度"存放於 time_list
-count = 0
-time_list = []
-for item in task_list:
-    obj = Event(item)
-    if (item == "離散"): count = Discrete_count
-    elif (item == "資結"): count = DS_count
-    elif (item == "計概"): count = CD_count
-    time_list.append(obj.GetTime(count))
-
-# output
-task_count =  len(task_list)
-print(f'今天有 {task_count} 項任務!\n')
-for i in range(task_count):
-    print(str(i+1) + ". " + task_list[i])
-    if time_list[i] == []:
-        print("")
-    else:
-        print(time_list[i])
-        print("")        
-print("")        
 input("Press the <Enter> key on the keyboard to exit.")
